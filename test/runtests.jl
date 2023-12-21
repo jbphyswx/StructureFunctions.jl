ed60612ec483ae3db45869e1d2550dd55729098b
Jordan Benjamin <jbenjami@clima.gps.caltech.edu>
2023-12-20 23:52:13 -0800

    updeps and formatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,11 @@ using Test
 
 
 @testset "StructureFunctions" begin
-    @assert StructureFunctions.Calculations.calculate_structure_function([1,2,3], [1,2,3], [1,2,3], 3, order=1)[1] == [0.0, 0.0, 0.0]
+    @assert StructureFunctions.Calculations.calculate_structure_function(
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        3,
+        order = 1,
+    )[1] == [0.0, 0.0, 0.0]
 end
