fac75407da2042782d86f694f61e47539f7a0ff4
Jordan Benjamin <jbenjami@clima.gps.caltech.edu>
2024-01-22 17:57:49 -0800

    climaformat

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,15 @@ using Test
 
 
 @testset "StructureFunctions" begin
-    a = StructureFunctions.StaticArrays.SVector( [1,2,3]...);
-    b = StructureFunctions.StaticArrays.SVector( [0,0,0]...);
+    a = StructureFunctions.StaticArrays.SVector([1, 2, 3]...)
+    b = StructureFunctions.StaticArrays.SVector([0, 0, 0]...)
     @assert StructureFunctions.Calculations.calculate_structure_function(
-        (a,a),
-        (b,b),
+        (a, a),
+        (b, b),
         1,
         Main.StructureFunctions.StructureFunctionTypes.LongitudinalSecondOrderStructureFunction();
-        show_progress=true,
-        verbose=true,
-        bin_spacing=:linear
+        show_progress = true,
+        verbose = true,
+        bin_spacing = :linear,
     )[1] == [0.0]
 end
