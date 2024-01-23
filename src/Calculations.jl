04d7421cbe3d3a68060ee6a6e64904622718c5ee
Jordan Benjamin <jbenjami@clima.gps.caltech.edu>
2024-01-22 17:05:41 -0800

    update with initial Structure Function formulation

--- src/Calculations.jl
+++ src/Calculations.jl
@@ -6,7 +6,22 @@ import ProgressMeter: @showprogress
 import Distances # from JuliaStats
 import ..HelperFunctions
 
-export calculate_structure_function
+using StaticArrays
+using LinearAlgebra
+
+using SharedArrays
+
+using ..StructureFunctionTypes # how can we make these natively available?
+# print(eval(Symbol("StructureFunctionTypes.SecondOrderStructureFunction"))) # doesn't work
+
+# using LoopVectorization # can't use w/ [bin] indexing below, see https://github.com/JuliaSIMD/LoopVectorization.jl/issues/331
+
+export calculate_structure_function, parallel_calculate_structure_function
+
+
+###########
+# Add methods for dispatching vectors/tuples to the static SVector versions...
+########
 
 """
 Consider using some sort of Intervals thingy for the intervals
@@ -16,50 +31,115 @@ Consider using some sort of Intervals thingy for the intervals
     - probably shouldn't use sharedarrays as those can possibly fail w/ concurrent reads/writes
 """
 function calculate_structure_function(
-    X::Vector,
-    Y::Vector,
-    values::Vector,
-    distance_bins::Vector{Tuple{FT, FT}};
-    order = 2,
-    distance_metric = Distances.Euclidean(),
-) where {FT}
+    x_vecs::NTuple{N, <:Union{SharedVector{FT1}, SVector{N2, FT1}}}, # Tuple{Vararg{Vector{FT},N}} # I think this is faster than array cause then we have columns only
+    u_vecs::NTuple{N, <:Union{SharedVector{FT2}, SVector{N2, FT2}}}, # Tuple{Vararg{Vector{FT},N}}
+    distance_bins::SVector{N3, Tuple{FT3, FT3}},
+    structure_function_type::StructureFunctionTypes.AbstractStructureFunctionType; # add N here?
+    distance_metric::Distances.PreMetric = Distances.Euclidean(),
+    verbose = true,
+    show_progress = true,
+    return_sums_and_counts = false,
+) where {FT1 <: Real, FT2 <: Real, FT3 <: Real, N, N2, N3}
     # calculate and bin and mean the pairwise distances
     # distances = pairwise(distance_metric, X, Y, dims=2) # will blow up if too big... so we're just doing the loop
-    # bin the distances
 
-    # preallocate output as vector of length of distance_bins
-    output = zeros(length(distance_bins))
-    counts = zeros(length(distance_bins))
 
-    @assert length(X) == length(Y) == length(values) # for speed, X,Y, and could could be put in an array to skip this check...
-    n = length(X)
+    # preallocate output as vector of length of distance_bins (vector so it's mutable)
+    output = zeros(N3)
+    counts = zeros(N3)
 
-    # distance_bins_vec = Tuple([[x[1] for x in distance_bins] ; [distance_bins[end][2]] ]) # the start of each bin plus the ending
-    distance_bins_vec = [[x[1] for x in distance_bins]; [distance_bins[end][2]]] # the start of each bin plus the ending
+    distance_bins_vec = SVector{length(distance_bins) + 1, FT3}(
+        [[distance_bin[1] for distance_bin in distance_bins]; [distance_bins[end][2]]]...,
+    ) # the start of each bin plus the ending
 
-    @info("calculating structure function")
-    @showprogress for i in 1:n
-        for j in 1:n
-            # find the bin that this distance belongs to
-            distance = distance_metric([X[i], Y[i]], [X[j], Y[j]])
-            bin = HelperFunctions.digitize(distance, distance_bins_vec) # this will fail when the the distance value equals the largest bin's largest edge due to the way digitize works, we could add a handler here to put that value back into the largest bin...
-            output[bin] += (values[i] - values[j])^order
-            counts[bin] += 1
-        end
+    if verbose
+        @info("calculating structure function")
     end
-    counts[counts .== 0] .= NaN
-    output ./= counts
-    return output, distance_bins
+
+
+    iter_inds = eachindex(x_vecs[1]) # these should all match..., idk if doing 1:N2 is faster but the indexing could be shifted...
+    @showprogress enabled = show_progress for i in iter_inds # is this the fast order?
+        _output, _counts = calculate_structure_function_i(
+            i,
+            x_vecs,
+            u_vecs,
+            distance_bins_vec,
+            structure_function_type;
+            distance_metric = distance_metric,
+        )
+        output .+= _output
+        counts .+= _counts
+
+        # X1 = SVector{N,FT1}((x_vec[i] for x_vec in x_vecs)...) # StaticArrays.sacollect(SVector{N, FT}, x_vec[i] for x_vec in x_vecs ) could be faster
+        # U1 = SVector{N,FT2}((u_vec[i] for u_vec in u_vecs)...)
+        # @simd for j in iter_inds
+        #     if i != j # this has δx and δu = 0, so we skip it
+        #         X2 = SVector{N,FT1}((x_vec[j] for x_vec in x_vecs)...)
+        #         U2 = SVector{N,FT2}((u_vec[j] for u_vec in u_vecs)...)
+
+        #         @inbounds distance = distance_metric(X1, X2) # this is the slow part
+        #         bin = HelperFunctions.digitize(distance, distance_bins_vec) # this will fail when the the distance value equals the smallest bin's smallest edge due to the way digitize works, we could add a handler here but we extended the smallest bin to be the next smallest float so this shouldn't happen
+        #         @inbounds output[bin] += structure_function_type.method(U2 - U1, HelperFunctions.r̂(X1, X2))
+        #         @inbounds counts[bin] += 1
+        #     end
+        # end
+    end
+
+    if return_sums_and_counts # just return the sums and the counts, don't take the mean in each bin...
+        return (output, counts), distance_bins
+    else # do the mean in each bin.
+        counts[counts .== 0] .= NaN # replace 0s with NaNs to avoid divide by 0 giving Inf
+        output ./= counts
+        return output, distance_bins
+    end
 end
 
+
+
+function calculate_structure_function_i(
+    i::Int,
+    x_vecs::NTuple{N, <:Union{SharedVector{FT1}, SVector{N2, FT1}}}, # Tuple{Vararg{Vector{FT},N}} # I think this is faster than array cause then we have columns only
+    u_vecs::NTuple{N, <:Union{SharedVector{FT2}, SVector{N2, FT2}}}, # Tuple{Vararg{Vector{FT},N}}
+    distance_bins_vec::SVector{N3, FT3},
+    structure_function_type::StructureFunctionTypes.AbstractStructureFunctionType; # add N here?
+    distance_metric::Distances.PreMetric = Distances.Euclidean(),
+) where {FT1 <: Real, FT2 <: Real, FT3 <: Real, N, N2, N3}
+
+
+    # preallocate output as vector of length of distance_bins (vector so it's mutable)
+    output = zeros(N3 - 1)
+    counts = zeros(N3 - 1)
+
+
+    iter_inds = eachindex(x_vecs[1]) # these should all match..., idk if doing 1:N2 is faster but the indexing could be shifted...
+    X1 = SVector{N, FT1}((x_vec[i] for x_vec in x_vecs)...) # StaticArrays.sacollect(SVector{N, FT}, x_vec[i] for x_vec in x_vecs ) could be faster
+    U1 = SVector{N, FT2}((u_vec[i] for u_vec in u_vecs)...)
+    @simd for j in iter_inds
+        if i != j # this has δx and δu = 0, so we skip it
+            X2 = SVector{N, FT1}((x_vec[j] for x_vec in x_vecs)...)
+            U2 = SVector{N, FT2}((u_vec[j] for u_vec in u_vecs)...)
+
+            @inbounds distance = distance_metric(X1, X2) # this is the slow part
+            bin = HelperFunctions.digitize(distance, distance_bins_vec) # this will fail when the the distance value equals the smallest bin's smallest edge due to the way digitize works, we could add a handler here but we extended the smallest bin to be the next smallest float so this shouldn't happen
+            @inbounds output[bin] += structure_function_type.method(U2 - U1, HelperFunctions.r̂(X1, X2))
+            @inbounds counts[bin] += 1
+        end
+    end
+    return output, counts
+end
+
+
 function calculate_structure_function(
-    X::Vector,
-    Y::Vector,
-    values::Vector,
-    distance_bins::Int;
-    order = 1,
-    distance_metric = Distances.Euclidean(),
-) where {FT}
+    x_vecs::NTuple{N, <:Union{SharedVector{FT1}, SVector{N2, FT1}}}, # Tuple{Vararg{Vector{FT},N}} # I think this is faster than array cause then we have columns only
+    u_vecs::NTuple{N, <:Union{SharedVector{FT2}, SVector{N2, FT2}}}, # Tuple{Vararg{Vector{FT},N}}
+    distance_bins::Int,
+    structure_function_type::StructureFunctionTypes.AbstractStructureFunctionType;
+    distance_metric::Distances.PreMetric = Distances.Euclidean(),
+    bin_spacing = :logarithmic,
+    verbose = true,
+    show_progress = true,
+    return_sums_and_counts = false,
+) where {FT1 <: Real, FT2 <: Real, N, N2} # You can't dispatch on N, N2 since they're Ints not types
     """
     Here we assume that the distance bins are evenly spaced
     However, we assume we cant store all the output pairs in memory (cause goes as len(x)^2
@@ -67,18 +147,81 @@ function calculate_structure_function(
     """
     # calculate and bin and mean the pairwise distances
     # distances = pairwise(distance_metric, X, Y, dims=2) # will blow up if too big... so we're just doing the loop
-    # bin the distances
+
 
     min_distance, max_distance = Inf, 0
-    @assert length(X) == length(Y) == length(values) # for speed, X,Y, and could could be put in an array to skip this check...
-    n = length(X)
+    n_distance_bins = distance_bins # save the number of bins since we'll overwrite distance_bins with the actual bins later.
 
     # calculate the bins
-    @info("Calculating min and max distances and generating bins")
-    @showprogress for i in 1:n
-        for j in 1:n
+    if verbose
+        @info("Calculating min and max distances and generating bins")
+    end
+
+    iter_inds = eachindex(x_vecs[1]) # these should all match..., idk if doing 1:N2 is faster but the indexing could be shifted...
+    @showprogress enabled = show_progress for i in iter_inds # is this the fast order?
+        _min_distance, _max_distance = minmax_i(i, x_vecs, distance_metric)
+        min_distance = min(min_distance, _min_distance)
+        max_distance = max(max_distance, _max_distance)
+
+        # X1 = SVector{N,FT1}((x_vec[i] for x_vec in x_vecs)...) # StaticArrays.sacollect(SVector{N, FT}, x_vec[i] for x_vec in x_vecs ) could be faster
+        # @simd for j in iter_inds
+        #     if i != j # don't compare to self (will add a distance of 0 to our distances)
+        #         X2 = SVector{N,FT1}((x_vec[j] for x_vec in x_vecs)...)
+        #         # update the min and max distances
+        #         @inbounds distance = distance_metric(X1, X2) # this is the slow part
+        #         if distance < min_distance
+        #             min_distance = distance
+        #         elseif distance > max_distance
+        #             max_distance = distance
+        #         end
+        #     end
+        # end
+    end
+
+    min_distance = prevfloat(min_distance) # go to the next smallest float from the min distance to make sure the true smallest distance can fit in the first bin (note this is needed so things matching min_distance don't get assigned bin '0', alternative is to check for bin 0 every time... which sounds slower
+    if bin_spacing == :linear
+        distance_bins = range(min_distance, max_distance, length = n_distance_bins + 1) # +1 to get the right number of bins since these are the edges
+    elseif bin_spacing ∈ (:logarithmic, :log)
+        distance_bins = 10 .^ range(log10(min_distance), log10(max_distance), length = n_distance_bins + 1) # +1 to get the right number of bins since these are the edges
+        distance_bins[1] = min_distance # combat floating point errors (may have rounded up or down during operations)
+        distance_bins[end] = max_distance # combat floating point errors (may have rounded up or down during operations)
+    else
+        error("bin_spacing must be :linear or :logarithmic/:log")
+    end
+    FT3 = eltype(distance_bins)
+    distance_bins = SVector{n_distance_bins, Tuple{FT3, FT3}}(
+        [(distance_bins[i], distance_bins[i + 1]) for i in 1:n_distance_bins]...,
+    ) # convert to tuples of the bin edges
+    return calculate_structure_function(
+        x_vecs,
+        u_vecs,
+        distance_bins,
+        structure_function_type;
+        distance_metric = distance_metric,
+        verbose = verbose,
+        show_progress = show_progress,
+        return_sums_and_counts = return_sums_and_counts,
+    )
+end
+
+
+function minmax_i(
+    i::Int,
+    x_vecs::NTuple{N, <:Union{SharedVector{FT}, SVector{N2, FT}}}, # Tuple{Vararg{Vector{FT},N}} # I think this is faster than array cause then we have columns only
+    distance_metric = Distances.Euclidean(),
+) where {FT <: Real, N, N2} # You can't dispatch on N, N2 since they're Ints not types
+    """ calculate, bin, and mean the pairwise distances """
+
+
+    # preallocate output as vector of length of distance_bins
+    min_distance, max_distance = Inf, 0
+
+    @inbounds X1 = SVector{N, FT}((x_vec[i] for x_vec in x_vecs)...) # StaticArrays.sacollect(SVector{N, FT}, x_vec[i] for x_vec in x_vecs ) could be faster
+    for j in eachindex(x_vecs[1])
+        if i != j
+            @inbounds X2 = SVector{N, FT}((x_vec[j] for x_vec in x_vecs)...) # StaticArrays.sacollect(SVector{N, FT}, x_vec[i] for x_vec in x_vecs ) could be faster
             # update the min and max distances
-            distance = distance_metric([X[i], Y[i]], [X[j], Y[j]])
+            distance = distance_metric(X1, X2) # this is the slow part
             if distance < min_distance
                 min_distance = distance
             elseif distance > max_distance
@@ -86,11 +229,14 @@ function calculate_structure_function(
             end
         end
     end
-    min_distance = prevfloat(min_distance) # go to the next smallest float from the min distance to make sure the true smallest distance can fit in the first bin
-    distance_bins = range(min_distance, max_distance, length = distance_bins + 1) # +1 to get the right number of bins since these are the edges
-    distance_bins = [(distance_bins[i], distance_bins[i + 1]) for i in 1:(length(distance_bins) - 1)] # convert to tuples of the bin edges
-
-    return calculate_structure_function(X, Y, values, distance_bins; order = order, distance_metric = distance_metric)
+    return min_distance, max_distance
 end
+
+
+
+
+
+function parallel_calculate_structure_function end # placeholder for parallel extension
+
 
 end
--- src/HelperFunctions.jl
+++ src/HelperFunctions.jl
@@ -3,9 +3,13 @@ Generic helper functions for the other routines in this package
 """
 module HelperFunctions
 
-export digitize
+using LinearAlgebra: dot, cross, norm, normalize
 
-function digitize(x::Real, bins::Vector{<:Real})
+using StaticArrays: SVector, @SVector
+
+export digitize, δu_longitudinal, δu_l, δu_transverse, δu_t
+
+function digitize(x::Real, bins::Union{Vector{<:Real}, SVector{N2, <:Real}}) where {N2}
     """
     Return the index of the bin that x belongs to
     (see np.digitize and https://discourse.julialang.org/t/find-the-index-of-a-bin-where-a-value-between-two-bin-value/32080/2?u=jbphyswx )
@@ -14,7 +18,10 @@ function digitize(x::Real, bins::Vector{<:Real})
     searchsortedfirst(bins, x) - 1
 end
 
-function digitize(x::Vector, bins::Vector{<:Real})
+function digitize(
+    x::Union{Vector{<:Real}, SVector{N, <:Real}},
+    bins::Union{Vector{<:Real}, SVector{N2, <:Real}},
+) where {N, N2}
     """
     Return the indices of the bins that x belongs to
     (see np.digitize and https://discourse.julialang.org/t/find-the-index-of-a-bin-where-a-value-between-two-bin-value/32080/2?u=jbphyswx )
@@ -22,7 +29,7 @@ function digitize(x::Vector, bins::Vector{<:Real})
     digitize.(Ref(bins), x)
 end
 
-function digitize(x::Vector, bins::Tuple{<:Real})
+function digitize(x::Union{Vector{<:Real}, SVector{N, <:Real}}, bins::Tuple{<:Real}) where {N}
     """
     Return the indices of the bins that x belongs to
     (see np.digitize and https://discourse.julialang.org/t/find-the-index-of-a-bin-where-a-value-between-two-bin-value/32080/2?u=jbphyswx )
@@ -30,4 +37,81 @@ function digitize(x::Vector, bins::Tuple{<:Real})
     digitize.(Ref(bins), x)
 end
 
+
+
+
+function δr(
+    x1::Union{Vector{FT}, SVector{N, FT}},
+    x2::Union{Vector{FT2}, SVector{N, FT2}},
+) where {FT <: Real, FT2 <: Real, N}
+    """
+    Return the vector from  x to y
+    """
+    return x2 .- x1
 end
+
+function r̂(
+    x1::Union{Vector{FT}, SVector{N, FT}},
+    x2::Union{Vector{FT}, SVector{N, FT2}},
+) where {FT <: Real, FT2 <: Real, N}
+    """
+    Return the unit vector from  x to y
+    """
+    return normalize(δr(x1, x2))
+end
+
+
+
+function δu_longitudinal(
+    δu::Union{Vector{FT}, SVector{N, FT}},
+    r_hat::Union{Vector{FT2}, SVector{N, FT2}},
+) where {FT <: Real, FT2 <: Real, N}
+    """
+    Return the longitudinal component of u (along the vector)
+    Left to the user to ensure r_hat has norm 1
+    """
+    return dot(δu, r_hat)
+end
+
+
+function δu_transverse(
+    δu::Union{Vector{FT}, SVector{N, FT}},
+    r_hat::Union{Vector{FT}, SVector{N, FT2}};
+    local_unit_vertical = [0, 0, 1],
+) where {FT <: Real, FT2 <: Real, N}
+    """
+    Return the transverse component of u (perpendicular to the vector)
+    Left to the user to ensure r_hat (and local_unit_vertical) has norm 1
+
+    -- note, it appears these two methods turned out to be identical -- see if we can simplify...
+    """
+
+    if isa(δu, SVector)
+        δu = SVector{length(δu)}(δu) # convert to SVector bc cross is defined in 2D and 3D for StaticArrays but only 3D for normal Arrays
+    end
+    if isa(r_hat, SVector)
+        r_hat = SVector{length(r_hat)}(r_hat)
+    end
+
+    if isnothing(local_unit_vertical)
+        return δu .- δu_longitudinal(δu, r_hat) * r_hat # not sure, i think you're supposed to project onto a direction perpendicular to k
+    else
+        _cross = cross(δu, r_hat)
+        if length(_cross) == 1 # if 2d, staticarrays just returns a scalar.... the k̂ in the third dimension is just 1 so ignore dot...
+            return _cross
+        else
+            return dot(local_unit_vertical, cross(δu, r_hat)) # cross is only defined for 3d vectors --- cross on 2d vectors is implemented in StaticArrays though...
+        end
+    end
+end
+
+
+δu_l = δu_longitudinal
+δu_t = δu_transverse
+
+
+
+
+
+
+end # end Module
