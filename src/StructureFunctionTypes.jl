80422414a240f88a64be7e75eb42f4075506f333
Jordan Benjamin <jbenjami@sampo.gps.caltech.edu>
2026-03-15 19:52:44 -0700

    fix: correct SF method wiring and helper function logic, remove manifest (Commit A5)

--- src/StructureFunctionTypes.jl
+++ src/StructureFunctionTypes.jl
@@ -5,15 +5,42 @@ using ..HelperFunctions
 
 abstract type AbstractStructureFunctionType end
 
+using LoopVectorization
+
+# norm2_reg(x) = norm(x)^2 # faster? NO
+# norm_alt(x) = sqrt(sum(val->val^2,x)) # faster? NO
+
+# # norm2_alt(x) = sum(val->val^2,x) # faster? NO
+# squared = val->val^2
+# norm2_alt(x) = @tturbo sum(squared,x) # faster? NO
+
+
+@inline @fastmath @inbounds norm2(x) = begin # no allocations, is a lil faster (for only 2 dimensions not much faster but)
+    out::eltype(x) = zero(eltype(x))
+    @tturbo warn_check_args=false for i in eachindex(x)
+        @inbounds @fastmath out +=  x[i]^2
+    end
+    return out
+end
+
+# # Something like this could reduce allocations further but pos[1] doesn't work w/ llvm and idk how else to do it, and it doesn't save thaaat much time I suppose
+# @inline @fastmath @inbounds norm2!(x, pos) = begin # no allocations, is a lil faster (for only 2 dimensions not much faster but)
+#     for i in eachindex(x)
+#         @inbounds @fastmath pos[] +=  x[i]^2
+#     end
+# end
+
 
 export SecondOrderStructureFunction,
     LongitudinalSecondOrderStructureFunction,
     TransverseSecondOrderStructureFunction,
     RotationalSecondOrderStructureFunction,
-    DivergenceSecondOrderStructureFunction,
+    DivergentSecondOrderStructureFunction,
     ThirdOrderStructureFunction,
     DiagonalConsistentThirdOrderStructureFunction,
+    DiagonalInconsistentThirdOrderStructureFunction,
     OffDiagonalInconsistentThirdOrderStructureFunction,
+    OffDiagonalConsistentThirdOrderStructureFunction,
     get_structure_function_type
 
 get_structure_function_type(x::String) = get_structure_function_type(Symbol(x)) # methods to get structure function instance from string or symbol
@@ -21,41 +48,65 @@ get_structure_function_type(x::Symbol) = eval(x)()
 
 # see https://doi.org/10.1002/2016GL069405 for definitions
 
+# The anonymous fcns might be slow and recompile every time, put them outside the struct for precompilation/consstruction only once once
 
 # 2nd Order
+# @inbounds SecondOrderStructureFunction_method(δu, r̂, pos) = norm2!(δu, pos) # if we could figure out an inplace solution we could reduce allocations a lot, but harder for other types of SFs...
+@inbounds SecondOrderStructureFunction_method(δu, r̂) = norm2(δu) 
+
 struct SecondOrderStructureFunction <: AbstractStructureFunctionType # D2 |δu|² = δu·δu = = δu_l·δu_l + δu_t·δu_t = δu_l² + δu_t² = D2l + D2t
     method::Function
-    SecondOrderStructureFunction() = new((δu, r̂) -> norm(δu)^2)
+    SecondOrderStructureFunction() = new(SecondOrderStructureFunction_method) # norm2 is slightly faster but isn't coming through in full runs for some reason...
+    # SecondOrderStructureFunction() = new((δu, r̂) -> norm(δu)^2)
 end
 
+@inbounds LongitudinalSecondOrderStructureFunction_method(δu, r̂) = mδu_l(δu, r̂)^2
 struct LongitudinalSecondOrderStructureFunction <: AbstractStructureFunctionType # D2L |δu_l|² = u_l·δu_l
     method::Function
-    LongitudinalSecondOrderStructureFunction() = new((δu, r̂) -> norm(δu_l(δu, r̂))^2)
+    LongitudinalSecondOrderStructureFunction() = new(LongitudinalSecondOrderStructureFunction_method)
 end
 
+@inbounds TransverseSecondOrderStructureFunction_method(δu, r̂) = norm2(δu_t(δu, r̂))  # maybe faster than mδu_l(δu, r̂)^2 since we never calculate n̂
 struct TransverseSecondOrderStructureFunction <: AbstractStructureFunctionType # D2T |δu_t|² = u_t·δu_t
     method::Function
-    TransverseSecondOrderStructureFunction() = new((δu, r̂) -> norm(δu_t(δu, r̂))^2)
+    TransverseSecondOrderStructureFunction() = new(TransverseSecondOrderStructureFunction_method)
 end
 
 struct RotationalSecondOrderStructureFunction <: AbstractStructureFunctionType end # NotImplemented
-struct DivergenceSecondOrderStructureFunction <: AbstractStructureFunctionType end # NotImplemented
+struct DivergentSecondOrderStructureFunction <: AbstractStructureFunctionType end # NotImplemented
 
 
 # 3rd Order
+# Note, we use mδu_{} instead of |δu_{}| because the magnitudes have a sign (relative to normal vector)! (I think this is the right way to do it, see https://doi.org/10.1002/2016GL069405 and old Lindberg and Cho papers)
+
+ThirdOrderStructureFunction_method(δu, r̂) = norm(δu)^3  # i think sqrt and then ³ is fastest since we have to calculate sqrt qnyway
 struct ThirdOrderStructureFunction <: AbstractStructureFunctionType # |δu|³  = |δu| δu·δu
     method::Function
-    ThirdOrderStructureFunction() = new((δu, r̂) -> norm(δu)^3)
+    ThirdOrderStructureFunction() = new(ThirdOrderStructureFunction_method)
 end
 
-struct DiagonalConsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # δu_l³
+DiagonalConsistentThirdOrderStructureFunction_method(δu, r̂) = mδu_l(δu, r̂)^3
+struct DiagonalConsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # mδu_l³ 
     method::Function
-    DiagonalConsistentThirdOrderStructureFunction() = new((δu, r̂) -> norm(δu_l(δu, r̂))^3)
+    DiagonalConsistentThirdOrderStructureFunction() = new(DiagonalConsistentThirdOrderStructureFunction_method)
 end
 
-struct OffDiagonalInconsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # δu_l**2 * δu_t
+DiagonalInconsistentThirdOrderStructureFunction_method(δu, r̂) = mδu_l(δu, r̂)^2 * mδu_t(δu, r̂)  # you could probably cache some intermediate results here
+struct DiagonalInconsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # |δu_l|² * mδu_t
     method::Function
-    OffDiagonalInconsistentThirdOrderStructureFunction() = new((δu, r̂) -> norm(δu_l(δu, r̂))^2 * norm(δu_t(δu, r̂)))
+    DiagonalInconsistentThirdOrderStructureFunction() = new(DiagonalInconsistentThirdOrderStructureFunction_method )
+end
+
+OffDiagonalInconsistentThirdOrderStructureFunction_method(δu, r̂) = mδu_l(δu, r̂) * norm2(δu_t(δu, r̂))  # norm(δu_t) might be faster than mδu_t(δu, r̂)^2 since we never calculate n̂ and we're squaring anyway so sign is irrelevant
+struct OffDiagonalInconsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # mδu_l * |δu_t|²
+    method::Function
+    OffDiagonalInconsistentThirdOrderStructureFunction() = new(OffDiagonalInconsistentThirdOrderStructureFunction_method) 
+end
+
+OffDiagonalConsistentThirdOrderStructureFunction_method(δu, r̂) = mδu_t(δu, r̂)^3  # you could probably cache some intermediate results here
+struct OffDiagonalConsistentThirdOrderStructureFunction <: AbstractStructureFunctionType # mδu_t³
+    method::Function
+    OffDiagonalConsistentThirdOrderStructureFunction() = new(OffDiagonalConsistentThirdOrderStructureFunction_method)
 end
 
 
--- test/test_core_correctness.jl
+++ test/test_core_correctness.jl
@@ -5,7 +5,7 @@ using Test
 using StaticArrays
 
 @testset "Core Correctness - Block A" begin
-    @testset "Commit A1: Blocked Path Regression" begin
+    @testset "Blocked path regression" begin
         x = ([0.0, 1.0], [0.0, 0.0])
         u = ([1.0, 2.0], [0.0, 0.0])
         bins = SVector(((0.0, 2.0),))
@@ -13,7 +13,7 @@ using StaticArrays
         @test_nowarn calculate_structure_function(x, u, bins, sf_type, verbose=false, show_progress=false)
     end
 
-    @testset "Commit A3: Pair-count Verification" begin
+    @testset "Pair-count verification" begin
         # N=3 points -> N(N-1)/2 = 3 pairs
         x = ([0.0, 1.0, 2.0], [0.0, 0.0, 0.0])
         u = ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
@@ -31,7 +31,7 @@ using StaticArrays
         @test sum(counts4) == 6.0
     end
 
-    @testset "Commit A4: Numerical Reference (Tiny Case)" begin
+    @testset "Numerical reference (Tiny case)" begin
         # 2 points: (0,0) and (1,0)
         # Velocities: (1,0) and (2,0)
         # du = (1, 0), rhat = (1, 0)
@@ -57,5 +57,28 @@ using StaticArrays
         u3 = ([0.0, 1.0, 0.0], [0.0, 0.0, 1.0])
         val3, _ = calculate_structure_function(x3, u3, bins, sf_type, verbose=false, show_progress=false)
         @test val3[1] ≈ 4/3
+    end
+
+    @testset "SF wiring and signed magnitude consistency" begin
+        # Test case: 2 points at (0,0) and (1,0) -> rhat = (1, 0), nhat = (0, -1)
+        # Velocities: (0,1) and (0,2) -> du = (0,1) [Transverse]
+        x = ([0.0, 1.0], [0.0, 0.0])
+        u = ([0.0, 0.0], [1.0, 2.0])
+        bins = SVector(((0.0, 2.0),))
+        
+        # Longitudinal Second Order: (du.rhat)^2 = 0^2 = 0
+        @test calculate_structure_function(x, u, bins, LongitudinalSecondOrderStructureFunction(), verbose=false, show_progress=false)[1][1] == 0.0
+        
+        # Transverse Second Order: |du_t|^2 = (-1)^2 = 1
+        @test calculate_structure_function(x, u, bins, TransverseSecondOrderStructureFunction(), verbose=false, show_progress=false)[1][1] == 1.0
+        
+        # Diagonal Consistent Third Order (l^3): 0^3 = 0
+        @test calculate_structure_function(x, u, bins, DiagonalConsistentThirdOrderStructureFunction(), verbose=false, show_progress=false)[1][1] == 0.0
+        
+        # Off-Diagonal Consistent Third Order (t^3): (-1)^3 = -1
+        @test calculate_structure_function(x, u, bins, OffDiagonalConsistentThirdOrderStructureFunction(), verbose=false, show_progress=false)[1][1] == -1.0
+        
+        # Off-Diagonal Inconsistent Third Order (l*t^2): 0 * (-1)^2 = 0
+        @test calculate_structure_function(x, u, bins, OffDiagonalInconsistentThirdOrderStructureFunction(), verbose=false, show_progress=false)[1][1] == 0.0
     end
 end
