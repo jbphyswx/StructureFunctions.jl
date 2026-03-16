2dc296ecf7843d5b5ebbe485747a39b534861173
Jordan Benjamin <jbenjami@sampo.gps.caltech.edu>
2026-03-15 19:35:56 -0700

    test: add baseline correctness and micro-bench scripts

---
+++ test/baseline_correctness.jl
@@ -0,0 +1,16 @@
+using StructureFunctions
+using StructureFunctions.StructureFunctionTypes
+using StructureFunctions.Calculations
+using Test
+using StaticArrays
+
+@testset "Baseline Correctness" begin
+    # Test case: 2 points, 1 bin
+    # If the blocker is present, this should throw an ErrorException
+    x = ([0.0, 1.0], [0.0, 0.0]) # NTuple of vectors
+    u = ([1.0, 2.0], [0.0, 0.0])
+    bins = SVector(((0.0, 2.0),))
+    sf_type = LongitudinalSecondOrderStructureFunction()
+
+    @test_throws ErrorException calculate_structure_function(x, u, bins, sf_type)
+end
